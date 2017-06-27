76102bc91255cb70e9d3371a0abaa22151a9c4c4
yanglin <richenlin@gmail.com>
2017-06-27 18:08:30 +0800

    更换模板

--- src/config/middleware.js
+++ src/config/middleware.js
@@ -7,6 +7,9 @@ module.exports = {
     config: { //中间件配置 
         static: {
             cache: false
+        },
+        router: {
+            deny_controller: ['base'], //禁止访问的控制器
         }
     }
 };
