49d26c76029aca213540d87ca33e9976a52e9952
richen <richenlin@gmail.com>
2019-12-02 16:49:37 +0800

    Fix build

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 #关闭调试
 #cp -rf ./index.js ./index.js.bak
-sed -i "s/.*app_debug:.*/app_debug: false,/" ./index.js
+#sed -i "s/.*app_debug:.*/app_debug: false,/" ./index.js
 
 #应用端口
-#cp -rf ./src/config/config.js ./src/config/config.js.bak
-sed -i "s/.*app_port:.*/app_port: 3000,/" ./src/config/config.js
+#cp -rf ./app/config/config.js ./app/config/config.js.bak
+#sed -i "s/.*app_port:.*/app_port: 3000,/" ./app/config/config.js
